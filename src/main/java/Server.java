d786213507b22c31bbbc7608c2d2bd50cc9b8cfd
PGK79 <pgk_nev@mail.ru>
2022-09-13 23:55:53 +0300

    Клиент и Сервер в разных классах

---
+++ src/main/java/Server.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Сервер запущен");
+        int port = 8089;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    System.out.printf("Установлено новое соединение. Port: %d%n", clientSocket.getPort());
+
+                    final String name = in.readLine();
+                    out.println(String.format("Привет %s, твой Port %d", name, clientSocket.getPort()));
+                }
+            }
+        }
+    }
+}
